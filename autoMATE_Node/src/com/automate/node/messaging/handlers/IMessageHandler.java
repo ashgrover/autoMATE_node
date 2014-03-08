ad4789b0a3be68bcd7a7888fad46ff9aac90b421
ian.k@me.com <ian.k@me.com>
2014-03-08 14:53:50 -0700

    More base arch.

--- autoMATE_Node/src/com/automate/node/messaging/handlers/IMessageHandler.java
+++ autoMATE_Node/src/com/automate/node/messaging/handlers/IMessageHandler.java
@@ -1,5 +1,15 @@
 package com.automate.node.messaging.handlers;
 
-public interface IMessageHandler {
-	private IStatusManager iStatusManager;
+import com.automate.protocol.Message;
+import com.automate.protocol.server.ServerProtocolParameters;
+
+/**
+ * Delegate for handling incoming messages
+ * @author ian.kabeary
+ *
+ * @param <M> The type of message this delegate handles
+ * @param <Params> container class for additional parameters.
+ */
+public interface IMessageHandler<M extends Message<ServerProtocolParameters>, Params> {
+	
 }
