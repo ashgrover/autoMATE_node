5eb1795e363a236a715cc98db92c57f3f6ad8a27
ashgrover <ash.grover@hotmail.com>
2014-03-08 15:27:58 -0700

    Added HashMap

--- autoMATE_Node/src/com/automate/node/messaging/MessageManager.java
+++ autoMATE_Node/src/com/automate/node/messaging/MessageManager.java
@@ -1,10 +1,14 @@
 package com.automate.node.messaging;
 
+import java.util.HashMap;
+
 import com.automate.node.InitializationException;
 import com.automate.node.messaging.handlers.IMessageHandler;
 import com.automate.node.status.IStatusManager;
 import com.automate.protocol.Message;
+import com.automate.protocol.Message.MessageType;
 import com.automate.protocol.client.ClientProtocolParameters;
+import com.automate.protocol.server.ServerProtocolParameters;
 
 public class MessageManager implements IMessageManager {
 
@@ -12,7 +16,8 @@ public class MessageManager implements IMessageManager {
 	private IMessageReceiver messageReceiver;
 	private IMessageSender messageSender;
 	private IMessageHandler messageHandler;
-	private IStatusManager statusManager;
+	//private IStatusManager statusManager;
+	private HashMap<MessageType, IMessageHandler<? extends Message<ServerProtocolParameters>,?>> handlers;
 	
 	@Override
 	public void initialize() throws InitializationException{
