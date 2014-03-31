caa1d9a5e023f520cb2d2a1ce91454d47c409096
ian.k@me.com <ian.k@me.com>
2014-03-31 16:19:20 -0600

    Added methods to StatusManager. Work in progress..

--- autoMATE_Node/src/com/automate/node/managers/status/StatusManager.java
+++ autoMATE_Node/src/com/automate/node/managers/status/StatusManager.java
@@ -1,25 +1,36 @@
 package com.automate.node.managers.status;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.automate.node.managers.IListener;
 import com.automate.node.managers.ManagerBase;
 import com.automate.node.managers.connection.IConnectionManager;
 import com.automate.node.managers.message.IMessageManager;
+import com.automate.node.utilities.FanGpioUtility;
 import com.automate.protocol.Message;
 import com.automate.protocol.client.ClientProtocolParameters;
 import com.automate.protocol.models.Status;
+import com.automate.protocol.models.Type;
+import com.automate.protocol.node.messages.NodeStatusUpdateMessage;
 import com.automate.protocol.server.ServerProtocolParameters;
 
 public class StatusManager extends ManagerBase<StatusListener> implements IStatusManager {
 
 	private IMessageManager messageManager;
 	private IConnectionManager connectionManager;
+	private FanGpioUtility fanGpioUtility;
+	private int nodeId;
+	private ClientProtocolParameters cpp;
 	
 	private StatusManager(IMessageManager messageManager, IConnectionManager connectionManager) {
 		super(StatusListener.class);
 		this.messageManager = messageManager;
 		this.connectionManager = connectionManager;
+		this.fanGpioUtility = new FanGpioUtility();
+		
+		this.nodeId = 0;
+		this.cpp = messageManager.getProtocolParameters();
 	}
 
 	/////////////////////////////////////////////////////////////////////////////////////////////////
@@ -72,8 +83,35 @@ public class StatusManager extends ManagerBase<StatusListener> implements IStatu
 	
 	@Override
 	public void onStatusUpdateRequested() {
-		// TODO Auto-generated method stub
-
+		
+		int speed = fanGpioUtility.getFanSpeed();
+		Status<?> currentStatus = null;
+		
+		// get the current status
+		
+		if (speed == 0) {
+			currentStatus = Status.newStatus("Table Fan", Type.INTEGER, 0);
+			
+		} else if (speed == 1) {
+			currentStatus = Status.newStatus("Table Fan", Type.INTEGER, 1);
+			
+		} else if (speed == 2) {
+			currentStatus = Status.newStatus("Table Fan", Type.INTEGER, 2);
+			
+		} else if (speed == 3) {
+			currentStatus = Status.newStatus("Table Fan", Type.INTEGER, 3);
+			
+		}
+		
+		List<Status<?>> statusList = new ArrayList<Status<?>>();
+		statusList.add(currentStatus);
+		
+		// TODO
+		this.cpp = null;
+		NodeStatusUpdateMessage updateMessage = new NodeStatusUpdateMessage(this.cpp, this.nodeId , statusList);
+		
+		messageManager.sendMessage(updateMessage);
+		
 	}
 
 	@Override
